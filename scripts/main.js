c7e508e519da4530072a78a69bc3f441ab3a9bbd
jewell86 <jewellsmailbox@gmail.com>
2018-07-16 09:00:02 -0700

    add skeleton layout of all pages

--- scripts/main.js
+++ scripts/main.js
@@ -18,9 +18,32 @@ document.querySelector('.login-submit').addEventListener('click', (ev) => {
     document.querySelector('.login-form').classList.add('hide')
     document.querySelector('.main-navbar').classList.add('hide')
     document.querySelector('.tasks-navbar').classList.remove('hide')
+    document.querySelector('.tasks-container').classList.remove('hide')
 })
 
-document.querySelector('.signup-submit').addEventListener('submit', (ev) => {
+document.querySelector('.signup-submit').addEventListener('click', (ev) => {
     ev.preventDefault()
+    document.querySelector('.signup-form').classList.add('hide')
+    document.querySelector('.login-form').classList.remove('hide')
+})
+
+document.querySelector('.new-list-button').addEventListener('click', (ev) => {
+    ev.preventDefault()
+    document.querySelector('.tasks-container').classList.add('hide')
+    document.querySelector('.new-list-form').classList.remove('hide')
+})
+
+document.querySelector('.new-list-submit').addEventListener('click', (ev) => {
+    ev.preventDefault()
+    document.querySelector('.new-list-form').classList.add('hide')
+    document.querySelector('.tasks-container').classList.remove('hide')
+})
+
+document.querySelector('.logout-button').addEventListener('click', (ev) => {
+    ev.preventDefault()
+    document.querySelector('.login-form').classList.remove('hide')
+    document.querySelector('.main-navbar').classList.remove('hide')
+    document.querySelector('.tasks-navbar').classList.add('hide')
+    document.querySelector('.tasks-container').classList.add('hide')
 })
 
